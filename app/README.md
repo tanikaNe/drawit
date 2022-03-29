0b52176d13473cd89f636da92d528c488d28e2b2
Weronika <weronikapios7@gmail.com>
2022-03-29 11:05:57 +0200

    set image as a background

---
+++ app/README.md
@@ -0,0 +1,5 @@
+download icon <a href="https://www.flaticon.com/free-icons/download" title="download icons">Download icons created by Debi Alpa Nugraha - Flaticon</a>
+share icon <a href="https://www.flaticon.com/free-icons/instagram-share" title="instagram share icons">Instagram share icons created by Freepik - Flaticon</a>
+undo icon <a href="https://www.flaticon.com/free-icons/undo" title="undo icons">Undo icons created by Creatype - Flaticon</a>
+redo icon <a href="https://www.flaticon.com/free-icons/redo" title="redo icons">Redo icons created by Creatype - Flaticon</a>
+erase icon <a href="https://www.flaticon.com/free-icons/eraser" title="eraser icons">Eraser icons created by Creartive - Flaticon</a>
--- app/src/main/java/com/gmail/weronikapios7/drawit/MainActivity.kt
+++ app/src/main/java/com/gmail/weronikapios7/drawit/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.gmail.weronikapios7.drawit
 
+import android.Manifest
 import android.app.Dialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.Gravity
 import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.core.view.get
 import com.gmail.weronikapios7.drawit.databinding.ActivityMainBinding
 
@@ -20,10 +31,37 @@ class MainActivity : AppCompatActivity() {
     private lateinit var drawingViewClass: DrawingView
     private lateinit var brushDialog: Dialog
     private var imageBtnCurrentPaint: ImageButton? = null
-    private val idRegx by lazy { Regex("color_[a-z]*").toPattern() }
+
+    private val openGalleryLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+        if(result.resultCode == RESULT_OK && result.data!=null){
+            binding.ivBackground.setImageURI(result.data?.data)
+        }
+    }
+
+    private val requestPerms: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if (isGranted) {
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show()
+                    val pickIntent =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    openGalleryLauncher.launch(pickIntent)
+                } else {
+                    if (permissionName == Manifest.permission.READ_EXTERNAL_STORAGE) {
+                        Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        hideSystemBars()
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
@@ -44,6 +82,10 @@ class MainActivity : AppCompatActivity() {
             brushDialog.show()
 
             findColorView(brushDialog)
+        }
+
+        binding.ibGallery.setOnClickListener {
+            requestStoragePerms()
         }
 
     }
@@ -81,6 +123,45 @@ class MainActivity : AppCompatActivity() {
             if (view.tag != null) {
                 paintClicked(view, dialog)
             }
+        }
+    }
+
+    private fun hideSystemBars() {
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+    }
+
+
+    private fun showRationaleDialog(title: String, message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+        builder.create().show()
+    }
+
+    private fun requestStoragePerms() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            showRationaleDialog("DrawIt!", "Needs to Access Your External Storage")
+        } else {
+            requestPerms.launch(
+                arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                    //TODO add writing external storage permission
+                )
+            )
         }
     }
 
